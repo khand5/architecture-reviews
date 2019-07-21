9b6b218cb5e28fa44d5cf9dcbe3f4c0d66e8d738
Danish Khan <khand5@mcmaster.ca>
2019-07-21 04:18:01 -0400

    Added preliminary navbar.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import "./App.css";
 import Clock from "./components/Clock/Clock";
 import LoginPanel from "./components/Panels/Login/LoginPanel";
 import ChessboardContainer from "./components/ChessboardContainer/ChessboardContainer";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">This is the header.</header>
+      <header className="App-header">
+        <Header />
+      </header>
       <body className="App-body">
         {" "}
         <Clock />
