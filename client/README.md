7fb7c4edf1dc6733525657f567c3d7db0b628711
Danish Khan <khand5@mcmaster.ca>
2019-10-01 23:15:07 -0400

    Added selector to search bar.

--- client/README.md
+++ client/README.md
@@ -66,3 +66,7 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+## Project Notes:
+
+1. JavaScript was used to implement the dropdown functionality in search form.
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -16,28 +16,35 @@ class Header extends React.Component {
           <h1>Global Architecture Reviews</h1>
         </div>
         <div className="Navbar-container">
-          <div className="Navbar-item Navbar-item-left Hover-highlight-border-bottom">
-            Secular
-          </div>
-          <div className="Navbar-item Navbar-item-left Hover-highlight-border-bottom">
-            Religious
-          </div>
-          <div className="Navbar-item Navbar-item-left Hover-highlight-border-bottom">
-            Traditional
-          </div>
+          <div className="Navbar-item Navbar-item-left Hover">Secular</div>
+          <div className="Navbar-item Navbar-item-left Hover">Religious</div>
+          <div className="Navbar-item Navbar-item-left Hover">Traditional</div>
 
-          <div className="Navbar-item-search Hover-highlight-border-bottom">
+          {/* Search Bar */}
+          <div className="Navbar-item-search Hover">
             <input
               type="text"
               className="Navbar-item-search-bar"
               placeholder="Search.."
             ></input>
-            <i className="Navbar-item-search-button fa fa-search"></i>
+            <a className="Navbar-item-search-button" href="">
+              <i className="fa fa-search"></i>
+            </a>
           </div>
 
-          <div className="Navbar-item Navbar-item-right Hover-highlight-border-bottom">
-            Login
-          </div>
+          <div className="Navbar-item Navbar-item-left">OR</div>
+
+          <select className="Navbar-item-select">
+            <option value="none">Rating...</option>
+            <option value="5">(5) World Wonder</option>
+            <option value="4">(4) Majestic</option>
+            <option value="3">(3) Excellent</option>
+            <option value="2">(2) Great</option>
+            <option value="1">(1) Good</option>
+          </select>
+
+          <div className="Navbar-item-rating"></div>
+          <div className="Navbar-item Navbar-item-right Hover">Login</div>
         </div>
       </div>
     );
